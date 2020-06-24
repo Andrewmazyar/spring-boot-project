56b8ab88525563181cbeb718f6deaa2f902edf97
Andrewmazyar <maziarandriy@gmail.com>
2020-06-24 09:55:56 +0300

    added file reader to project

---
+++ src/main/java/web/spring_boot/springbootproject/lib/FileReaderFromFile.java
@@ -0,0 +1,25 @@
+package web.spring_boot.springbootproject.lib;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileReaderFromFile {
+
+    public List<String> getStringFromFile(String path) throws IOException {
+        List<String>records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                records.add(line);
+            }
+        } catch (IOException e) {
+            throw new IOException("No such File", e);
+        }
+        return records;
+    }
+}
